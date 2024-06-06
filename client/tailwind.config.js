659349c5bcc7e95fee95a0a96a9b8c725bd58dda
Arntit Zonia <arditzonia@gmail.com>
2024-06-06 22:57:00 +0100

    feat: installed additional style modules

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,10 @@
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{tsx,ts}"],
+  content: ["./src/**/*.{tsx,ts}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };

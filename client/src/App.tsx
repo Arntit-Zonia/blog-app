35f55b22b7063247047694fe2c394e5b10075ba4
Arntit Zonia <arditzonia@gmail.com>
2024-06-06 21:56:54 +0100

    feat: setup view routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
 import { FC } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import About from "./views/About";
+import SignUp from "./views/SignUp";
+import SignIn from "./views/SignIn";
+import Home from "./views/Home";
+import Dashboard from "./views/Dashboard";
+import Projects from "./views/Projects";
 
 export const App: FC = () => {
-  return <div>Blog App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;

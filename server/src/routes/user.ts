e774a950ae28379464721f3525a23e6ca28b2cde
Arntit Zonia <arditzonia@gmail.com>
2024-06-10 00:32:46 +0100

    feat: created POST signup route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,13 +1,18 @@
 import express, { Router, Request, Response } from "express";
 
 import errorHandler from "../controllers/errorHandler";
+import User from "../models/users";
 
 const router: Router = express.Router();
 
-router.get(
-  "/",
+router.post(
+  "/signup",
   errorHandler(async (req: Request, res: Response) => {
-    res.send("Hello, World!");
+    const user = new User(req.body);
+
+    await user.save();
+
+    res.status(201).send(user);
   })
 );
 

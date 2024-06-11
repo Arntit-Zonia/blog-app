edf8a9922d79931de5ce2596be3e9d63265ebaca
Arntit Zonia <arditzonia@gmail.com>
2024-06-11 23:44:43 +0100

    feat: created SignUp component

--- client/src/views/SignUp.tsx
+++ client/src/views/SignUp.tsx
@@ -1,7 +1,48 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
 
 const SignUp: FC = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:texxt-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              AZ
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">You can sign up using your email and password or google account</p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Username" />
+              <TextInput type="text" placeholder="Username" />
+            </div>
+            <div>
+              <Label value="Email" />
+              <TextInput type="text" placeholder="Email" />
+            </div>
+            <div>
+              <Label value="Password" />
+              <TextInput type="text" placeholder="Password" />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span className="">Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;

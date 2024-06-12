5ccf1a9c18912aa4ac277d0c9d2499c60814b110
Arntit Zonia <arditzonia@gmail.com>
2024-06-12 01:43:40 +0100

    refactor: refactored Form so it's a controlled component

--- client/src/views/SignUp.tsx
+++ client/src/views/SignUp.tsx
@@ -1,31 +1,45 @@
-import { FC, FormEvent } from "react";
+import { FC, useState, ChangeEvent, FormEvent } from "react";
 import { Link } from "react-router-dom";
 import { Button, Label, TextInput } from "flowbite-react";
-
 import axios from "axios";
 
 const SignUp: FC = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   const handleFormSubmit = async (e: FormEvent) => {
     try {
       e.preventDefault();
 
-      const formElement = e.currentTarget as HTMLFormElement;
+      const { username, email, password } = formData;
 
-      const formData = new FormData(formElement);
+      if (!username || !email || !password) {
+        return console.error("Please fill in all fields");
+      }
 
-      const username = formData.get("Username") as string;
-      const email = formData.get("Email") as string;
-      const password = formData.get("Password") as string;
-
-      if (!username || !email || !password) return console.error("Please fill in all fields");
-
-      await axios.post("http://localhost:3000/signup", {
+      await axios.post("/signup", {
         username,
         email,
         password,
       });
 
-      formElement.reset();
+      setFormData({
+        username: "",
+        email: "",
+        password: "",
+      });
     } catch (error) {
       console.error(error);
     }
@@ -35,7 +49,7 @@ const SignUp: FC = () => {
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         <div className="flex-1">
-          <Link to="/" className="font-bold dark:texxt-white text-4xl">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               AZ
             </span>
@@ -47,15 +61,27 @@ const SignUp: FC = () => {
           <form className="flex flex-col gap-4" onSubmit={handleFormSubmit}>
             <div>
               <Label value="Username" />
-              <TextInput type="text" placeholder="Username" name="Username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Email" />
-              <TextInput type="text" placeholder="Email" name="Email" />
+              <TextInput type="text" placeholder="Email" name="email" value={formData.email} onChange={handleChange} />
             </div>
             <div>
               <Label value="Password" />
-              <TextInput type="text" placeholder="Password" name="Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
             </div>
             <Button gradientDuoTone="purpleToPink" type="submit">
               Sign Up

2a17f4a5cecc38ce2e355478e533f7962de8f238
Arntit Zonia <arditzonia@gmail.com>
2024-06-15 18:50:49 +0100

    feat: imported new schema file to index.ts

--- server/src/models/user/index.ts
+++ server/src/models/user/index.ts
@@ -1,8 +1,8 @@
-import { Schema, Model, model } from "mongoose";
-
-import validator from "validator";
+import { Model, model } from "mongoose";
 
 import { IUserDocument } from "../../interfaces/user";
+
+import userSchema from "./schema";
 
 import "./methods";
 import "./statics";
@@ -11,42 +11,6 @@ import "./hooks";
 interface IUserModel extends Model<IUserDocument> {
   findByCredentials(email: string, password: string): IUserDocument;
 }
-
-export const userSchema = new Schema<IUserDocument>(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      validate(value: string) {
-        if (!validator.isEmail(value)) {
-          throw new Error("Please provide a valid email address");
-        }
-      },
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 7,
-      validate(value: string) {
-        if (value.toLowerCase().includes("password")) {
-          throw new Error("Password cannot contain 'password'");
-        }
-      },
-    },
-    tokens: [{ token: { type: String, required: true } }],
-  },
-  { timestamps: true }
-);
 
 const User = model<IUserDocument, IUserModel>("User", userSchema);
 

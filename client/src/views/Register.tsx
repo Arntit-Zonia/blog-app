75eecbdf062709d01b6711e5552cb66736c5921a
Arntit Zonia <arditzonia@gmail.com>
2024-06-18 20:51:18 +0100

    feat: implemented WelcomeMessage comp to Register and Login comps

--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import UserForm from "../components/UserForm";
+import WelcomeMessage from "../components/WelcomeMessage";
 
 import { IUserFormData } from "../interfaces/user";
 
@@ -17,22 +18,14 @@ const Register: FC = () => {
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        {/* TODO: Create owner blog component and find a better name for it */}
+        <WelcomeMessage messageType="register" />
         <div className="flex-1">
-          <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              AZ
-            </span>
-            Blog
-          </Link>
-          <p className="text-sm mt-5">You can sign up using your email and password or google account</p>
-        </div>
-        <div className="flex-1">
+          {/* <p className="text-sm mt-5">You can sign up using your email and password or google account</p> */}
           <UserForm formType="register" onSubmit={handleRegisterSubmit} />
           <div className="flex gap-2 text-sm mt-5">
             <span className="">Have an account?</span>
-            <Link to="/login" className="text-blue-500">
-              Sign In
+            <Link to="/user/login" className="text-blue-500">
+              Login
             </Link>
           </div>
         </div>

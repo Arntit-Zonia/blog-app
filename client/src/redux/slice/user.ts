4c6b1106de7333f613c26c7eaf4d29032bf9309b
Arntit Zonia <arditzonia@gmail.com>
2024-06-19 19:37:06 +0100

    fix: removed destructuring as it prevented state from updating

--- client/src/redux/slice/user.ts
+++ client/src/redux/slice/user.ts
@@ -1,35 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-import { IUser } from "../../interfaces/user";
+import { IUserFormData } from "../../interfaces/user";
 
 interface IUserState {
-  currentUser: IUser | null;
-  error: string | null;
-  loading: boolean;
+  currentUser: IUserFormData | null;
+  errorMessage: string | null;
+  isLoading: boolean;
 }
 
 const initialState: IUserState = {
   currentUser: null,
-  error: null,
-  loading: false,
+  errorMessage: null,
+  isLoading: false,
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    loginStart: ({ loading, error }) => {
-      loading = true;
-      error = null;
+    loginStart: (state) => {
+      state.isLoading = true;
+      state.errorMessage = null;
     },
-    loginSuccess: ({ currentUser, loading }, action: PayloadAction<IUser>) => {
-      currentUser = action.payload;
-      loading = false;
+    loginSuccess: (state, action: PayloadAction<IUserFormData>) => {
+      state.currentUser = action.payload;
+      state.isLoading = false;
     },
-    loginFailure: ({ error, loading }, action: PayloadAction<string>) => {
-      error = action.payload;
-      loading = false;
+    loginFailure: (state, action: PayloadAction<string>) => {
+      state.errorMessage = action.payload;
+      state.isLoading = false;
     },
   },
 });

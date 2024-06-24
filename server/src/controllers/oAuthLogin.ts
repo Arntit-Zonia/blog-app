59437198cf844f850b4593c2ec02bcd5ce5feb97
Arntit Zonia <arditzonia@gmail.com>
2024-06-25 00:32:10 +0100

    feat: created OAuth controller

---
+++ server/src/controllers/oAuthLogin.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+
+import User from "../models/user";
+
+const OAuthLogin = async (req: Request, res: Response): Promise<void> => {
+  console.log("body", req.body);
+
+  const existingUser = await User.findOne({ email: req.body.email });
+
+  if (existingUser) {
+    res.status(404).send("User already exists");
+
+    return;
+  }
+
+  const { username, email, tokens } = req.body;
+
+  const user = new User({
+    username,
+    email,
+    // initial google account password
+    password: Math.random().toString(36).slice(-8),
+    tokens,
+  });
+
+  await user.save();
+
+  res.status(201).send(user);
+};
+
+export default OAuthLogin;

9dfaee40ca494257a253597495e3ef01d3a51572
Arntit Zonia <arditzonia@gmail.com>
2024-06-25 00:32:31 +0100

    feat: implemented OAuth controller to /oath/login route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -3,10 +3,12 @@ import { Router } from "express";
 import errorHandler from "../controllers/errorHandler";
 import register from "../controllers/register";
 import login from "../controllers/login";
+import OAuthLogin from "../controllers/oAuthLogin";
 
 const router: Router = Router();
 
 router.post("/register", errorHandler(register));
 router.post("/login", errorHandler(login));
+router.post("/oath/login", errorHandler(OAuthLogin));
 
 export default router;

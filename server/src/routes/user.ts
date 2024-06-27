bde404d0ecce4dfd99654a612052693991ab5281
Arntit Zonia <arditzonia@gmail.com>
2024-06-27 23:28:21 +0100

    feat: implemented logout controller to route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import errorHandler from "../controllers/errorHandler";
 import register from "../controllers/register";
 import login from "../controllers/login";
+import logout from "../controllers/logout";
 import OAuthLogin from "../controllers/oAuthLogin";
 
 const router: Router = Router();
@@ -10,5 +11,6 @@ const router: Router = Router();
 router.post("/register", errorHandler(register));
 router.post("/login", errorHandler(login));
 router.post("/oath/login", errorHandler(OAuthLogin));
+router.post("/user/logout", errorHandler(logout));
 
 export default router;

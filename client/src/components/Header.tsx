5f87fec79526a0c81839cfb7d867b9378f5d7205
Arntit Zonia <arditzonia@gmail.com>
2024-06-27 21:58:38 +0100

    feat: conditionally rendering header user profile info

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
-
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa6";
 
+import { RootState } from "../redux/store";
+
 const Header: FC = () => {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state: RootState) => state.user);
 
   return (
     <Navbar className="border-b-2">
@@ -26,11 +29,23 @@ const Header: FC = () => {
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/user/login">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Login
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown arrowIcon={false} inline label={<Avatar alt="user" img={currentUser.profilePicture} rounded />}>
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+          </Dropdown>
+        ) : (
+          <Link to="/user/login">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Login
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>

eddad090ed451c32786346bc450257ee278e3404
Arntit Zonia <arditzonia@gmail.com>
2024-06-27 22:30:55 +0100

    refactor: cleanup the proxy setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/register": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
-      "/login": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
-      "/oath/login": {
-        target: "http://localhost:3000",
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const SERVER_PORT = `http://localhost:${env.VITE_SERVER_PORT}`;
+
+  return {
+    server: {
+      proxy: {
+        "/register": {
+          target: SERVER_PORT,
+          secure: false,
+        },
+        "/login": {
+          target: SERVER_PORT,
+          secure: false,
+        },
+        "/oath/login": {
+          target: SERVER_PORT,
+          secure: false,
+        },
+        "/user/logout": {
+          target: SERVER_PORT,
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });

ef5a48854c426c773f2f1f9b7ecacd68c5b27868
Arntit Zonia <arditzonia@gmail.com>
2024-07-01 18:50:52 +0100

    feat: added the currentToken type to the IUserDocument interface

--- server/src/interfaces/user.ts
+++ server/src/interfaces/user.ts
@@ -6,6 +6,7 @@ export interface IUserDocument extends Document {
   email: string;
   password: string;
   tokens: { token: string }[];
+  currentToken: string;
   generateAuthToken(): Promise<string>;
   profilePicture: string;
 }

77c1ed608f3c76f7cffe90b173dc152d50836316
Arntit Zonia <arditzonia@gmail.com>
2024-07-02 20:01:27 +0100

    feat: generating jwt token on user registration

--- server/src/controllers/register.ts
+++ server/src/controllers/register.ts
@@ -4,10 +4,11 @@ import User from "../models/user";
 
 const register = async (req: Request, res: Response): Promise<void> => {
   const user = new User(req.body);
+  const token = await user.generateAuthToken();
 
   await user.save();
 
-  res.status(201).send(user);
+  res.status(201).send({ user, token });
 };
 
 export default register;

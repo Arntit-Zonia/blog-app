e87c7dd190d4e0c26d2df808c2a25716ceb4781f
Arntit Zonia <arditzonia@gmail.com>
2024-07-02 01:28:46 +0100

    feat: created Sidebar component

---
+++ client/src/components/dashboard/Sidebar.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Sidebar as SidebarWrapper } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+
+import axios from "axios";
+
+import { logout } from "../../redux/slice/user";
+import { RootState } from "../../redux/store";
+
+import useQueryParam from "../../hooks/useQueryParam";
+
+const Sidebar: FC = () => {
+  const dispatch = useDispatch();
+  const tab = useQueryParam("tab");
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
+  const handleLogout = async (): Promise<void> => {
+    dispatch(logout());
+
+    await axios.post("/user/logout", currentUser);
+  };
+
+  return (
+    <SidebarWrapper className="w-full md:w-56">
+      <SidebarWrapper.Items>
+        <SidebarWrapper.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <SidebarWrapper.Item active={tab === "profile"} icon={HiUser} label="User" labelColor="dark">
+              Profile
+            </SidebarWrapper.Item>
+          </Link>
+          <SidebarWrapper.Item icon={HiArrowSmRight} className="cursor-pointer" onClick={handleLogout}>
+            Logout
+          </SidebarWrapper.Item>
+        </SidebarWrapper.ItemGroup>
+      </SidebarWrapper.Items>
+    </SidebarWrapper>
+  );
+};
+
+export default Sidebar;

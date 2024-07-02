a4dec60dd15bd568ac38ec2013a7c68a0f11360b
Arntit Zonia <arditzonia@gmail.com>
2024-07-02 20:01:51 +0100

    feat: added auth middleware to /logout route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -5,12 +5,13 @@ import register from "../controllers/register";
 import login from "../controllers/login";
 import logout from "../controllers/logout";
 import OAuth from "../controllers/oAuth";
+import auth from "../middleware/auth";
 
 const router: Router = Router();
 
 router.post("/register", errorHandler(register));
 router.post("/login", errorHandler(login));
 router.post("/oath/google", errorHandler(OAuth));
-router.post("/logout", errorHandler(logout));
+router.post("/logout", auth, errorHandler(logout));
 
 export default router;

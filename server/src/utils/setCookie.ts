2aee53dddca37329e6fe2312199a384c8877fd18
Arntit Zonia <arditzonia@gmail.com>
2024-07-03 23:59:18 +0100

    feat: created setTokenCookie util function

---
+++ server/src/utils/setCookie.ts
@@ -0,0 +1,12 @@
+import { Response } from "express";
+
+const setTokenCookie = (res: Response, token: string): void => {
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 3600000, // 1 hour
+  });
+};
+
+export default setTokenCookie;

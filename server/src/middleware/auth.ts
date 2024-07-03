1fe478a324297df90983220ffa72b1863dbade6c
Arntit Zonia <arditzonia@gmail.com>
2024-07-04 00:07:38 +0100

    feat: the auth middleware now gets the token from the cookies

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,18 +1,19 @@
 import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import { Response, NextFunction } from "express";
 
 import User from "../models/user";
-
 import { IRouteHandler } from "../interfaces/controllers";
+import { IAuthenticatedRequest } from "../interfaces/middleware";
 
-const auth: IRouteHandler = async (req, res, next) => {
+const auth: IRouteHandler = async (req: IAuthenticatedRequest, res: Response, next: NextFunction) => {
   try {
-    const token = req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.cookies.token;
     const secret = process.env.JWT_SECRET as Secret;
 
     if (!token || !secret) throw new Error(!token ? "No token provided" : "No secret provided");
 
     const verifiedToken = jwt.verify(token, secret) as JwtPayload;
-    const user = await User.findOne({ _id: verifiedToken, "tokens.token": token });
+    const user = await User.findOne({ _id: verifiedToken._id, "tokens.token": token });
 
     if (!user) {
       throw new Error("User not found");

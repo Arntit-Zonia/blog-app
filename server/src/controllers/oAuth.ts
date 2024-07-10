b09da708f81fe40fc5bb2a452d64d3a370747551
Arntit Zonia <arditzonia@gmail.com>
2024-07-11 00:28:20 +0100

    feat: updating token cookie for existing oauth users

--- server/src/controllers/oAuth.ts
+++ server/src/controllers/oAuth.ts
@@ -4,26 +4,21 @@ import User from "../models/user";
 import setTokenCookie from "../utils/setCookie";
 
 const OAuth = async (req: Request, res: Response): Promise<void> => {
-  console.log("body", req.body);
+  const { username, email, profilePicture, token, isOAuth } = req.body;
 
   const existingUser = await User.findOne({ email: req.body.email });
 
   if (existingUser) {
-    console.log("testing existingUser ====>", {
-      existingUserTokens: existingUser.tokens,
-      token: req.body.token,
-    });
-
     existingUser.tokens.push({ token: req.body.token });
 
     await existingUser.save();
+
+    setTokenCookie(res, "token", token);
 
     res.status(200).send(existingUser);
 
     return;
   }
-
-  const { username, email, profilePicture, token, isOAuth } = req.body;
 
   const user = new User({
     username,

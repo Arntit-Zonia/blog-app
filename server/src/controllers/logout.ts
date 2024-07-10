63cd3bc8c5ce489f1256ca012d4a58f7c2fac484
Arntit Zonia <arditzonia@gmail.com>
2024-07-11 00:27:05 +0100

    feat: clearing token cookie after user token has been removed from db

--- server/src/controllers/logout.ts
+++ server/src/controllers/logout.ts
@@ -11,15 +11,15 @@ const logout = async (req: IAuthenticatedRequest, res: Response): Promise<void> 
     return;
   }
 
+  user.tokens = user.tokens.filter((token) => token.token !== req.token);
+
+  await user.save();
+
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
   });
-
-  user.tokens = user.tokens.filter((token) => token.token !== req.token);
-
-  await user.save();
 
   res.status(200).send({ loggedOutUser: user });
 };
